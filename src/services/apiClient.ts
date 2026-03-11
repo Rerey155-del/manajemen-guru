bc80ef53dbbe729f9635cce571bd915794ad2277
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2026-03-11 21:46:41 +0700

    jalan

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Konfigurasi instance Axios untuk base URL API
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api', // Ganti dengan URL backend sesungguhnya di .env
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Ganti dengan URL backend sesungguhnya di .env
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
